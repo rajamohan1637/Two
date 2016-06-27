90e191e2d9bddd97fcde65a1877dc178e648bfa8
raju <raju@raju.com>
2016-06-27 15:23:08 -0400

    Two

---
+++ testapp/src/main/java/MainServlet.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, 
+		HttpServletResponse resp)
+		throws ServletException, IOException{
+	
+		resp.setContentType("text/html");
+		String html ="<html><body><strong><em>Hello World</em></strong></body></html>";
+		PrintWriter out = resp.getWriter();
+		out.println(html);
+
+	}
+}
